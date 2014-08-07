c436955feddcaf08eb19e92d6fdc2fa14c065862
hidelafoglia <gmail@hdknr.com>
2014-08-07 16:40:02 +0900

    Django 1.6

--- src/scripts/celery_workman.py
+++ src/scripts/celery_workman.py
@@ -18,18 +18,19 @@ if __name__ == '__main__':
     sys.path.append(os.path.dirname(sys.argv[1]))    #: Important
     sys.path.append(sys.argv[1])    #: Important
 
-    from django.core.management import execute_manager
     import imp
     try:
         imp.find_module('settings') # Assumed to be in the same directory.
     except ImportError:
         import sys
-        sys.stderr.write("Error: Can't find the file 'settings.py' in the directory containing %r. It appears you've customized things.\nYou'll have to run django-admin.py, passing it your settings module.\n" % __file__)
+        sys.stderr.write("""
+Error: Can't find the file 'settings.py' in the directory containing %r. 
+It appears you've customized things.
+AYou'll have to run django-admin.py, passing it your settings module.\n""" % __file__)
         sys.exit(1)
-    
-    import settings
+
     import workers
-    
-    if __name__ == "__main__":
-        sys.argv = [ os.path.abspath(sys.argv[0])] + workers.configure(*sys.argv) 
-        execute_manager(settings)
+    argv = [ os.path.abspath(sys.argv[0])] + workers.configure(*sys.argv)  
+
+    from django.core.management import execute_from_command_line
+    execute_from_command_line(argv)
